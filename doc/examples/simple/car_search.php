46dc6bca4b5c331faf05632acd02d4c4859f3931
Bence Nagy <bence@underyx.me>
2014-09-08 11:47:27 +0200

    Fix an accidentally renamed variable in car rental example

--- doc/examples/simple/car_search.php
+++ doc/examples/simple/car_search.php
@@ -1,5 +1,5 @@
 <?php
 
 $search_query = new Allmyles\Cars\SearchQuery('LHR', '2015-03-01', '2015-03-04');
-$hotels = $allmyles->searchCar($search_query)->get();
-var_dump($hotels);
+$cars = $allmyles->searchCar($search_query)->get();
+var_dump($cars);
